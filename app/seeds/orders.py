ffd24fcb200cfc273ca36fbee4e9b73b173e1cc3
Andrei Vorobev <andrey.vorob1995@gmail.com>
2023-11-17 17:32:37 -0500

    restaurants and orders seeds

---
+++ app/seeds/orders.py
@@ -0,0 +1,42 @@
+from app.models import db, Order, environment, SCHEMA
+from sqlalchemy.sql import text
+from datetime import datetime
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_orders():
+    demo = Order(
+        user_id = 1,
+        created_at = datetime.today,
+        is_complete = False,
+        address = '6431 Chicago Ave, Seattle, WA 12345',
+        price = 68.2,
+        restaurant_id = 2
+        )
+    demo2 = Order(
+        user_id = 1,
+        created_at = datetime.today,
+        is_complete = False,
+        address = '6431 Chicago Ave, Seattle, WA 12345',
+        price = 17.39,
+        restaurant_id = 2
+        )
+
+    db.session.add(demo)
+    db.session.add(demo2)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_orders():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.orders RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM orders"))
+        
+    db.session.commit()
--- app/seeds/restaurants.py
+++ app/seeds/restaurants.py
@@ -3,55 +3,55 @@ from sqlalchemy.sql import text
 
 
 # Adds a demo user, you can add other users here if you want
-def seed_Restaurant():
+def seed_restaurants():
     demo = Restaurant(
         owner_id = 1, 
-        name, 
-        category, 
-        address, 
-        city, 
-        state, 
-        hours_open, 
-        hours_close, 
-        preview_img, 
-        min_order_time, 
-        max_order_time)
+        name = 'Scooby Ice Cream', 
+        category = 'American', 
+        address = '111 California st', 
+        city = 'San Francisco', 
+        state = 'CA', 
+        hours_open = '09:00:00', 
+        hours_close = '20:00:00', 
+        preview_img = 'https://pbs.twimg.com/media/DfFRrlVWkAAYbAz.jpg:large', 
+        min_order_time = 10, 
+        max_order_time = 35)
     marnieRestaurant = Restaurant(
         owner_id = 2, 
-        name, 
-        category, 
-        address, 
-        city, 
-        state, 
-        hours_open, 
-        hours_close, 
-        preview_img, 
-        min_order_time, 
-        max_order_time)
+        name = 'Shaggy Burgers', 
+        category = 'American', 
+        address = '112 California st', 
+        city = 'San Francisco', 
+        state = 'CA', 
+        hours_open = '09:00:00', 
+        hours_close = '20:00:00',  
+        preview_img = 'https://static.wikia.nocookie.net/scoobydoo/images/a/a4/Hamburger.png/revision/latest?cb=20180119232637', 
+        min_order_time = 15, 
+        max_order_time = 24)
     bobbieRestaurant = Restaurant(
         owner_id = 3, 
-        name, 
-        category, 
-        address, 
-        city, 
-        state, 
-        hours_open, 
-        hours_close, 
-        preview_img, 
-        min_order_time, 
-        max_order_time)
+        name = "Fred's Pizza", 
+        category = 'Italian', 
+        address = '113 California st', 
+        city = 'San Francisco', 
+        state = 'CA', 
+        hours_open = '09:00:00', 
+        hours_close = '20:00:00', 
+        preview_img = 'https://static.wikia.nocookie.net/flinstones/images/a/a9/The_Flintstone_Comedy_Hour_-_Pizza-Puss.jpg/revision/latest?cb=20200305205945', 
+        min_order_time = 11, 
+        max_order_time = 42)
     bobbieRestaurant2 = Restaurant(
         owner_id = 4, 
-        name, 
-        category, 
-        address, 
-        city, 
-        state, 
-        hours_open, 
-        hours_close, 
-        preview_img, 
-        min_order_time, 
-        max_order_time)
+        name = 'Velma BBQ Grill', 
+        category = 'American', 
+        address = '114 California st', 
+        city = 'San Francisco', 
+        state = 'CA', 
+        hours_open = '09:00:00', 
+        hours_close = '20:00:00', 
+        preview_img ='https://64.media.tumblr.com/e74cd9b1e16b3a7f2971103e2a51548b/ff51ddbdee0567af-d9/s640x960/cfe8c90e5ea784c40647a436837fb51b85a73c7f.gif', 
+        min_order_time = 20, 
+        max_order_time = 70)
 
     db.session.add(demo)
     db.session.add(marnieRestaurant)
@@ -66,10 +66,10 @@ def seed_Restaurant():
 # incrementing primary key, CASCADE deletes any dependent entities.  With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
-def undo_users():
+def undo_restaurants():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.restaurants RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM users"))
+        db.session.execute(text("DELETE FROM restaurants"))
         
     db.session.commit()
