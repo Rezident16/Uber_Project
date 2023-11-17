dc02057c14d8c5057d39b1f06b7a264571983db0
Andrei Vorobev <andrey.vorob1995@gmail.com>
2023-11-17 15:39:30 -0500

    starter package + models combination

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
+from .db import db
+from .user import User
+from .db import environment, SCHEMA
 from flask_sqlalchemy import SQLAlchemy
 from .item import Item
 from .order import Order
@@ -5,5 +8,3 @@ from .restaurant import Restaurant
 from .review import Review
 from .items_likes import items_likes
 from .orders_items import orders_items
-
-db = SQLAlchemy()
