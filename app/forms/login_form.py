4c59c3bcd2a6839a923d7cd0800dc082b5035cd0
Zohaib <zohaibrajan@gmail.com>
2023-11-18 23:31:06 -0500

    order_items

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
+from app.models.user import User
+
 
 
 def user_exists(form, field):
