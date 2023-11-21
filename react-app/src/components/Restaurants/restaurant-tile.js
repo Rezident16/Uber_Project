d1d8c850730bca565b0de29659290e4e2283d975
Brian Stokes <bcstokes4@gmail.com>
2023-11-20 21:33:51 -0500

    time functionality

--- react-app/src/components/Restaurants/restaurant-tile.js
+++ react-app/src/components/Restaurants/restaurant-tile.js
@@ -5,13 +5,14 @@
 function RestaurantTile({ restaurant }) {
                 let hours = new Date().getHours()
                 let AMorPM = hours >= 12  && hours < 24? 'PM' : 'AM'
-                // if(hours === 12 || hours === 0) hours = 12
-                // else {
-                //     hours = AMorPM === 'PM' ? hours - 12 : hours
-                // }
                 let minutes = new Date().getMinutes()
                 if(minutes === 0 || minutes < 10) minutes = `0${minutes}`
                 let currentTime = `${hours}:${minutes}`
+
+                // if (Number(restaurant.hours_close.slice(0,2)) > 12) {
+                //     restaurant.hours_close.slice(0,2) = 
+                // }
+                
     return (
         <div>
             <p><span>{restaurant.name}</span> <span>({restaurant.address})</span></p>
