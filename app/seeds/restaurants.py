122b74be8ced2fdeab296fe9a7ceae916baf85d8
masonaustin42 <masonaustin42@gmail.com>
2023-11-22 14:38:36 -0800

    add to restaurants/items seeders

--- app/seeds/restaurants.py
+++ app/seeds/restaurants.py
@@ -53,11 +53,81 @@ def seed_restaurants():
         preview_img ='https://savoryscoot.s3.amazonaws.com/seeder-images/daphne-velma-hot-dog.gif',
         min_order_time = 20,
         max_order_time = 70)
+    mcdonalds = Restaurant(
+        owner_id = 5,
+        name = "McDonald's",
+        category = 'American',
+        address = '11 Main St',
+        city = 'San Francisco',
+        state = 'CA',
+        hours_open=datetime.strptime('06:00:00', '%H:%M:%S').time(),
+        hours_close=datetime.strptime('22:00:00', '%H:%M:%S').time(),
+        preview_img ='https://savoryscoot.s3.amazonaws.com/seeder-images/mcdonalds.png',
+        min_order_time = 10,
+        max_order_time = 25)
+    wendys = Restaurant(
+        owner_id = 6,
+        name = "Wendy's",
+        category = 'American',
+        address = '21 Second St',
+        city = 'San Francisco',
+        state = 'CA',
+        hours_open=datetime.strptime('06:00:00', '%H:%M:%S').time(),
+        hours_close=datetime.strptime('22:00:00', '%H:%M:%S').time(),
+        preview_img ='https://savoryscoot.s3.amazonaws.com/seeder-images/wendys.png',
+        min_order_time = 10,
+        max_order_time = 25)
+    chipotle = Restaurant(
+        owner_id = 7,
+        name = "Chipotle",
+        category = 'American',
+        address = '46599 Ankunding Dale',
+        city = 'San Francisco',
+        state = 'CA',
+        hours_open=datetime.strptime('10:00:00', '%H:%M:%S').time(),
+        hours_close=datetime.strptime('23:00:00', '%H:%M:%S').time(),
+        preview_img ='https://savoryscoot.s3.amazonaws.com/seeder-images/chipotle.png',
+        min_order_time = 10,
+        max_order_time = 25)
+    five_guys = Restaurant(
+        owner_id = 8,
+        name = "Five Guys",
+        category = 'American',
+        address = '4672 Hazel Grove',
+        city = 'San Francisco',
+        state = 'CA',
+        hours_open=datetime.strptime('12:00:00', '%H:%M:%S').time(),
+        hours_close=datetime.strptime('20:00:00', '%H:%M:%S').time(),
+        preview_img ='https://savoryscoot.s3.amazonaws.com/seeder-images/five-guys.png',
+        min_order_time = 10,
+        max_order_time = 25)
+    taco_bell = Restaurant(
+        owner_id = 1,
+        name = "Taco Bell",
+        category = 'Mexican',
+        address = '2960 Bernhard Station',
+        city = 'San Francisco',
+        state = 'CA',
+        hours_open=datetime.strptime('05:00:00', '%H:%M:%S').time(),
+        hours_close=datetime.strptime('23:00:00', '%H:%M:%S').time(),
+        preview_img ='https://savoryscoot.s3.amazonaws.com/seeder-images/tacobell.jpeg',
+        min_order_time = 10,
+        max_order_time = 25)
+
+
+
 
     db.session.add(scooby_restaurant)
     db.session.add(shaggy_restaurant)
     db.session.add(fred_restaurant)
     db.session.add(velma_restaurant)
+    db.session.add(mcdonalds)
+    db.session.add(wendys)
+    db.session.add(chipotle)
+    db.session.add(five_guys)
+    db.session.add(taco_bell)
+
+
     db.session.commit()
 
 
@@ -72,5 +142,6 @@ def undo_restaurants():
         db.session.execute(f"TRUNCATE table {SCHEMA}.restaurants RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM restaurants"))
+
 
     db.session.commit()
