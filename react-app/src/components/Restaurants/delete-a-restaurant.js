eea384cb9a1953a96c00c940687f3df7abe2fe9d
Zohaib <zohaibrajan@gmail.com>
2023-11-22 12:55:49 -0500

    Delete Restaurants functional

--- react-app/src/components/Restaurants/delete-a-restaurant.js
+++ react-app/src/components/Restaurants/delete-a-restaurant.js
@@ -2,20 +2,22 @@ import React from "react";
 import { useModal } from "../../context/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { fetchDeleteRestaurant } from "../../store/restaurant";
-import { fetchRestaurants } from "../../store/restaurants";
+import { deleteRestaurantThunk } from "../../store/restaurants";
+// import { fetchRestaurants } from "../../store/restaurants";
+import { removeRestaurant } from "../../store/restaurant";
 
 function DeleteARestaurantModal() {
   const history = useHistory();
   const dispatch = useDispatch();
   const { closeModal } = useModal();
-  const restaurant = useSelector(state => state.restaurant)
-  
+  const restaurant = useSelector((state) => state.restaurant);
+
   const deleteRestaurant = (e) => {
     e.preventDefault();
 
-    dispatch(fetchDeleteRestaurant(restaurant.id)).then(closeModal());
-    dispatch(fetchRestaurants());
+    dispatch(deleteRestaurantThunk(restaurant.id))
+      .then(dispatch(removeRestaurant))
+      .then(closeModal());
     history.push("/restaurants");
   };
 
@@ -25,10 +27,18 @@ function DeleteARestaurantModal() {
       <span id="delete-restaurant-span">
         Are you sure you want to remove this restaurant?
       </span>
-      <button className="confirm-delete" style={{cursor: "pointer"}}onClick={deleteRestaurant}>
+      <button
+        className="confirm-delete"
+        style={{ cursor: "pointer" }}
+        onClick={deleteRestaurant}
+      >
         Yes (Delete Restaurant)
       </button>
-      <button className="do-not-delete" style={{cursor: "pointer"}}onClick={closeModal}>
+      <button
+        className="do-not-delete"
+        style={{ cursor: "pointer" }}
+        onClick={closeModal}
+      >
         No (Keep Restaurant)
       </button>
     </div>
