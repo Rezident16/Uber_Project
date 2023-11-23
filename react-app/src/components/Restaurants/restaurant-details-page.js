69db01f227195094d0ba3091c8884e6f649d2ac0
masonaustin42 <masonaustin42@gmail.com>
2023-11-22 23:05:02 -0800

    restaurant details styles

--- react-app/src/components/Restaurants/restaurant-details-page.js
+++ react-app/src/components/Restaurants/restaurant-details-page.js
@@ -7,6 +7,7 @@ import RestaurantReviews from "../Reviews/reviews-for-restaurant";
 import CreateAReviewModal from "../Reviews/post-a-review";
 import OpenModalButton from "../OpenModalButton";
 import DeleteARestaurantModal from "./delete-a-restaurant";
+import "./restaurant-details.css";
 
 function RestaurantDetailPage() {
   const dispatch = useDispatch();
@@ -39,6 +40,18 @@ function RestaurantDetailPage() {
   if (minutes === 0 || minutes < 10) minutes = `0${minutes}`;
   let currentTime = `${hours}:${minutes}`;
 
+  function parseHours(time) {
+    let timeSplit = time.split(":");
+    console.log(parseInt(timeSplit[0]));
+    if (parseInt(timeSplit[0]) > 12) {
+      timeSplit[0] = `${parseInt(timeSplit[0]) - 12}`;
+      return timeSplit.join(":") + " PM";
+    } else {
+      timeSplit[0] = `${parseInt(timeSplit[0])}`;
+      return timeSplit.join(":") + " AM";
+    }
+  }
+
   if (user) {
     hasOrdered = orders?.some(
       (order) => order.user_id == user.id && order.restaurant_id == restaurantId
@@ -55,8 +68,8 @@ function RestaurantDetailPage() {
         <img src={restaurant.preview_img} alt=""></img>
         <h2>{restaurant.name}</h2>
         <div>
-          <i className="fa-solid fa-star"></i>
           <p>
+            <i className="fa-solid fa-star" style={{ fontSize: "12px" }}></i>
             {reviews.length
               ? (
                   reviews.reduce((curr, prev) => curr + prev.stars, 0) /
@@ -66,20 +79,35 @@ function RestaurantDetailPage() {
           </p>
           {reviews.length ? (
             <p>
-              {reviews.length} {reviews.length === 1 ? "Review" : "Reviews"}
+              ({reviews.length} {reviews.length === 1 ? "Rating" : "Ratings"})
             </p>
           ) : (
             <p>Be the first to leave a Review!!!</p>
           )}
-          {currentTime < restaurant.hours_close ? (
-            <p> Open until {restaurant.hours_close}</p>
-          ) : (
-            <p>closed</p>
-          )}
+
+          <span style={{ fontSize: "30px" }}> · </span>
+          <p>{restaurant.category}</p>
+          <span style={{ fontSize: "30px" }}> · </span>
+          <p>
+            <a href="#reviews" style={{ textDecoration: "underline" }}>
+              Read Reviews
+            </a>
+          </p>
+          <div className="hours">
+            {currentTime < restaurant.hours_close ? (
+              <p> Open until {parseHours(restaurant.hours_close)}</p>
+            ) : (
+              <p>Closed, opens at {parseHours(restaurant.hours_open)}</p>
+            )}
+          </div>
         </div>
       </header>
 
-      <RestaurantReviews restaurant={restaurant} />
+      <Items restaurant={restaurant} />
+
+      <div id="reviews">
+        <RestaurantReviews restaurant={restaurant} />
+      </div>
 
       {hasOrdered && hasNoReview && (
         <OpenModalButton
@@ -93,7 +121,6 @@ function RestaurantDetailPage() {
           modalComponent={<DeleteARestaurantModal />}
         />
       )}
-      <Items restaurant={restaurant} />
     </>
   );
 }
